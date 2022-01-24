6aa1425d9a658e954caf5e4ab1b63b5c938260a8
Lucas Dramowicz <ldramowicz@yahoo.com>
2022-01-24 13:08:23 -0500

    commit app to repo

--- README.md
+++ README.md
@@ -1,70 +1,39 @@
-# Getting Started with Create React App
+Tribe Weather App by Lucas
+=========
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+<p align="center">
+  <img width="683" height="376" src="./readme.png">
+</p>
 
-## Available Scripts
+### To clone from gitHub:
+```
+$ git clone https://github.com/ldramowicz/tribe-weather-app
+```
 
-In the project directory, you can run:
+### Installation
+To run this project, install it using npm:
+```
+$ cd tribe-weather-app
+$ npm install
+$ npm start
+```
+Finally, navigate to **http://localhost:3000** in browser
 
-### `npm start`
+Note: You can click on the cat/dog image in the card to see the full-sized version
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+### API
+* url - http://api.weatherbit.io/v2.0/forecast/daily
+* example request - https://api.weatherbit.io/v2.0/forecast/daily?city=Vancouver,CA&days=5&key=API_KEY
+* documentation- https://www.weatherbit.io/api/weather-forecast-16-day
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+### Objectives
+For this assignment we would like you to build a weather forecast page.\
+The user shall be able to search for a specific city. Once the city has been chosen, they will then be able to input the number of days into the future and the page will let the user know how the weather will be using pictures of cats and dogs.\
+For example, if the weather on those days is raining show a picture of a wet cat. If the weather is good show a picture of a warm dog. If the weather is going to snow show a picture of cat in snow.
 
-### `npm test`
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+### Tech
+To create the app I have used the following:
+* React with create-react-app as template
+* ant-design -> for page layout, cards and form elements
+* moment.js -> for date formatting
--- src/App.js
+++ src/App.js
@@ -1,23 +1,203 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+import 'antd/dist/antd.css';
 import './App.css';
+import moment from "moment";
+import {Alert, Button, Card, Form, Image, Input, InputNumber, Row, Spin} from 'antd';
 
 function App() {
+
+    //https://api.weatherbit.io/v2.0/forecast/daily?city=Raleigh,NC&days=1&key=4bae5c53d5fc4e6fb5e2cbae219959b3
+    //https://www.weatherbit.io/api/weather-forecast-16-day
+
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    // init number of forecast days to 5
+    const [numOfDays, setNumOfDays] = useState(5);
+    const [currentCity, setCurrentCity] = useState();
+    const [cityToDisplay, setCityToDisplay] = useState("");
+    const [numOfDaysToDisplay, setNumOfDaysToDisplay] = useState(0)
+    const [forecast, setForecast] = useState();
+
+    const MAX_FORECAST_DAYS = 10;
+    const API_KEY = '4bae5c53d5fc4e6fb5e2cbae219959b3';
+
+    const [form] = Form.useForm();
+
+    const animalPics = {
+        clear: 'https://www.wallpaperup.com/uploads/wallpapers/2015/10/11/818129/3a6e45bb774e0cc934f1986b828012a7.jpg',
+        cloudy: 'https://thumbs.dreamstime.com/b/close-up-beautiful-face-scottish-kitten-lying-clouds-close-up-beautiful-muzzle-milk-colored-scottish-217368947.jpg',
+        overcast: 'https://www.wallpaperflare.com/static/328/506/326/puppy-waiting-sky-collar-wallpaper.jpg',
+        rain: 'https://www.bestcat.com/assets/images/bestcat/Umbrella_Cats.jpg',
+        snow: 'https://static.stacker.com/s3fs-public/styles/sar_screen_maximum_large/s3/2019-12/00006276.png',
+        thunderstorm: 'https://s3.amazonaws.com/images.hamlethub.com/hh20mediafolder/1042/201506/thunderstorms---dogs.jpg',
+        other: 'https://www.petmd.com/sites/default/files/dog-cat-window-shutterstock_105936335.jpg'
+    }
+
+    const getForecastData = async() => {
+        try {
+            const response = await fetch(`https://api.weatherbit.io/v2.0/forecast/daily?city=${currentCity}&days=${numOfDays}&key=${API_KEY}`);
+            if (!response.ok || response.status === 204) {
+                throw new Error(
+                    `HTTP error occurred. Current response status is [${response.status}: ${response.statusText}]`
+                );
+            }
+            return await response.json();
+        } catch(err) {
+            setError(err.message);
+            setForecast(null);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const handleInputChange = (value) => {
+        setError(null);
+        setNumOfDays(value);
+    };
+
+    const handleCityChange = (e) => {
+        setError(null);
+        setCurrentCity(e.target.value)
+    }
+
+    const handleSubmit = () => {
+        setError(null);
+        // show loading icon
+        setLoading(true);
+        getForecastData()
+            .then(forecastData => {
+                // store forecast data
+                setForecast(forecastData.data);
+                // store city name and country code to display in title
+                setCityToDisplay(forecastData.city_name + ' (' + forecastData.country_code + ')');
+                setNumOfDaysToDisplay(numOfDays);
+                setError(null);
+            });
+    }
+
+    const formatDate = (date) => {
+        // format date to MON, TUE, WED etc.
+        return moment(date).format("ddd");
+    }
+
+    const showAnimalPic = (code) => {
+        // destructure animalPics object
+        const {clear, cloudy, overcast, rain, snow, thunderstorm, other}  = animalPics;
+        let currentWeatherType;
+
+        // switch based on the first character of the weather code (i.e. c01d)
+        switch(code.charAt(0)) {
+            case "c": {
+                // there is a number of cloud types starting with "c" so we need to make more distinctions
+                // for the weather types available
+                if (code === "c01d") {
+                    currentWeatherType = clear;
+                } else if (code === "c04d") {
+                    currentWeatherType = overcast;
+                }
+                else {
+                    currentWeatherType = cloudy;
+                }
+                break;
+                }
+            case "r": {
+                currentWeatherType = rain;
+                break;
+            }
+            case "s": {
+                currentWeatherType = snow;
+                break;
+            }
+            case "t": {
+                currentWeatherType = thunderstorm;
+                break;
+            }
+            default: {
+                // catch all other types not handled above
+                currentWeatherType = other;
+            }
+        }
+        return  currentWeatherType;
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Row justify="center">
+            <div>
+                <Form layout='inline' form={form} onFinish={handleSubmit}>
+
+                    {/* input city name */}
+                    <Form.Item label="City:"
+                               name="city"
+                               rules={[{ required: true }]}>
+                        <Input onChange={handleCityChange} placeholder="i.e. Montreal, CA" />
+                    </Form.Item>
+
+                    {/* input number of days to forecast */}
+                    <Form.Item label="Number of forecast days:">
+                        <InputNumber
+                            min={1}
+                            max={MAX_FORECAST_DAYS}
+                            value={numOfDays}
+                            onChange={handleInputChange}
+                        />
+                    </Form.Item>
+
+                    {/* submit */}
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
+        </Row>
+
+        {/* show spinner if loading data */}
+        {loading && <div><Spin /></div>}
+
+        {/* show API fetching errors, if any */}
+        {error && (
+            <Alert message={error} type="error" showIcon />
+        )}
+
+        {/* show weather cards if data is available */}
+        {forecast &&
+            <>
+                <Row justify="center">
+                    <div className="title">{numOfDaysToDisplay} day weather forecast for {cityToDisplay}</div>
+                </Row>
+                <Row justify="center">
+                    <div className="forecast-cards">
+
+                            {/* map through all the data available for X number of days (numOfDaysToDisplay) */}
+                            {forecast.slice(0,numOfDaysToDisplay).map(({valid_date, max_temp, min_temp, weather}, index) =>
+
+                                <Card key={index} title={formatDate(valid_date)} hoverable>
+                                    <Image
+                                        width={118}
+                                        height={75}
+                                        // src of the picture depends on the weather type code
+                                        // which we're getting from "weather.icon" property
+                                        src={showAnimalPic(weather.icon)}
+                                    />
+
+                                    {/*
+                                        uncomment lines below to see more forecast data in each card
+                                    */}
+
+                                    {/*<div className="description">{weather.description}</div>
+                                    <div className="description">{weather.icon}</div>*/}
+
+                                    {/*show min/max temp */}
+                                    <div className="maxTemp">{max_temp}&deg;C</div>
+                                    <div className="minTemp">{min_temp}&deg;C</div>
+                                </Card>
+                            )}
+                    </div>
+                </Row>
+            </>
+        }
     </div>
   );
 }
